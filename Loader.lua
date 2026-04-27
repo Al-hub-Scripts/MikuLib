b79ac1d9f33947e582c5f7dcb5e0e01050ca1d3c
Al-hub-Scripts <gabriellasbury@gmail.com>
2026-04-26 22:25:24 -0400

    Fetch themes and components from GitHub
    Load themes and components from GitHub links.

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,14 @@
+local github = "https://raw.githubusercontent.com/Al-hub-Scripts/MikuLib/refs/heads/main/"
 
+local links = {
+	githubTheme = github .. "Themes.lua",
+    githubComponents = github .. "components.lua"
+}
+
+
+local lib = {
+    themes = loadstring(game:HttpGet(links.githubTheme))(),
+    components = loadstring(game:HttpGet(links.githubComponents))()
+}
+
+return lib
